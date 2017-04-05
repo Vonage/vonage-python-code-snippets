39d15ca1d4485079db0eb2f9cdbe36bbb240d5fc
Tom Morris <tom@tommorris.org>
2017-04-05 13:48:04 +0100

    Creating DLR example

---
+++ sms/dlr-flask.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from pprint import pprint
+
+
+app = Flask(__name__)
+
+
+@app.route("/receipt", methods=['GET', 'POST'])
+def delivery_receipt():
+    if request.method == 'POST':
+        pprint(request.form)
+        pprint(request.get_json())
+    else:
+        pprint(dict(request.args))
+    return jsonify({})
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
+
