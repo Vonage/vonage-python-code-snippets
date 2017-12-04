3a84b7090d0046c60ae1636cf9732587489bae7f
Mark Smith <mark.smith@practicalpoetry.co.uk>
2017-12-04 12:48:49 +0000

    Add record a call example.

---
+++ voice/record_a_call.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import http
+from pprint import pprint
+
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route("/webhooks/answer")
+def answer_call():
+    for param_key, param_value in request.args.items():
+        print("{}: {}".format(param_key, param_value))
+
+    recording_webhook_url = request.url_root + "webhooks/recording"
+
+    return jsonify([
+        {
+            "action": "talk",
+            "text": "Please leave a message after the tone, then press pound."
+        },
+        {
+            "action": "record",
+            "endOnKey": "#",
+            "beepStart": "true",
+            "eventUrl": [recording_webhook_url]
+        },
+        {
+            "action": "talk",
+            "text": "Thank you for your message."
+        }
+    ])
+
+
+@app.route("/webhooks/recording", methods=['POST'])
+def recording_webhook():
+    pprint(request.get_json())
+    return ('', http.HTTPStatus.NO_CONTENT)
+
+
+if __name__ == '__main__':
+    app.run(port=3000)
