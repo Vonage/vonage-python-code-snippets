3617568b1c1e7cb13121e7ae795f571651a587fc
Sam Machin <smachin@Sams-iMac.local>
2018-04-30 13:47:18 +0100

    tidied up outbound call

--- voice/make-an-outbound-call.py
+++ voice/make-an-outbound-call.py
@@ -1,24 +1,16 @@
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
-
-dotenv_path = join(dirname(__file__), '../.env')
-load_dotenv(dotenv_path)
-
-APPLICATION_PRIVATE_KEY_PATH = os.getenv('APPLICATION_PRIVATE_KEY_PATH')
-APPLICATION_ID = os.getenv('APPLICATION_ID')
-TO_NUMBER = os.getenv('TO_NUMBER')
-NEXMO_NUMBER = os.getenv('NEXMO_NUMBER')
-
+#!/usr/bin/env python3
 import nexmo
+from pprint import pprint
 
 client = nexmo.Client(
     application_id=APPLICATION_ID,
     private_key=APPLICATION_PRIVATE_KEY_PATH,
 )
 
-client.create_call({
+response = client.create_call({
   'to': [{'type': 'phone', 'number': TO_NUMBER}],
   'from': {'type': 'phone', 'number': NEXMO_NUMBER},
   'answer_url': ['https://developer.nexmo.com/ncco/tts.json']
 })
+
+pprint(response)
