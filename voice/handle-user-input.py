c6ba873747a449308798c1c6c55d36a8f1e0f598
Sam Machin <smachin@Sams-iMac.local>
2018-04-30 13:14:01 +0100

    Added Handle User Input

---
+++ voice/handle-user-input.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+from flask import Flask, request, jsonify
+from pprint import pprint
+
+app = Flask(__name__)
+
+
+@app.route("/webhooks/answer")
+def answer_call():
+    for param_key, param_value in request.args.items():
+        print("{}: {}".format(param_key, param_value))
+    input_webhook_url = request.url_root + "webhooks/dtmf"
+    ncco =[
+        {
+            "action": "talk",
+            "text": "Hello, please press any key to continue."
+        },
+        {
+            "action": "input",
+            "maxDigits": 1,
+            "eventUrl": [input_webhook_url]
+        }
+    ]
+    return jsonify(ncco)
+
+
+
+@app.route("/webhooks/dtmf", methods=['POST'])
+def inout_webhook():
+    data = request.get_json()
+    pprint(data)
+    ncco =[
+        {
+            "action": "talk",
+            "text": "You pressed {}, goodbye".format(data['dtmf'])
+        }
+    ]
+    return jsonify(ncco)
+
+
+if __name__ == '__main__':
+    app.run(port=3000)
