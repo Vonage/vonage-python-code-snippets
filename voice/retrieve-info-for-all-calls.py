48a8309f22c59fe6e589fec346fa2bd809e83790
Tony Bedford <tony.bedford@vonage.com>
2018-05-02 14:26:33 +0100

    Ignore all private keys. Improved building block to comply more with spec.

--- voice/retrieve-info-for-all-calls.py
+++ voice/retrieve-info-for-all-calls.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 import nexmo
 from pprint import pprint
+from datetime import datetime, timedelta
 
 client = nexmo.Client(
     application_id=APPLICATION_ID,
     private_key=APPLICATION_PRIVATE_KEY_PATH,
 )
 
-# Note call can be made to current call or a completed call
-response = client.get_calls()
-pprint(response)
+DATE_END = datetime.utcnow().replace(microsecond=0).isoformat()+"Z"
+DATE_START = (datetime.utcnow() - timedelta(hours=24, minutes=00)).replace(microsecond=0).isoformat()+"Z"
+
+params = {"date_start": DATE_START, "date_end": DATE_END}
+response = client.get_calls(params)
+calls = response['_embedded']['calls']
+for call in calls:
+    pprint(call)
