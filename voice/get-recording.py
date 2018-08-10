cece1b787fa618ddf46c0d54acb490d25c7fa228
Tony Bedford <tony.bedford@vonage.com>
2018-08-10 12:51:57 +0100

    Get the specified recording.

---
+++ voice/get-recording.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import nexmo
+import time
+from pprint import pprint
+
+client = nexmo.Client(
+    application_id=APPLICATION_ID,
+    private_key=APPLICATION_PRIVATE_KEY_PATH,
+)
+
+response = client.get_recording(URL)
+pprint(response)
+
