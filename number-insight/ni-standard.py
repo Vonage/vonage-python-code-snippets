8b16f553e395f38e326ebfa536830016182c8eec
Mark Lewin <mark.lewin@vonage.com>
2018-10-03 13:36:14 +0100

    Add basic, standard and advanced (sync) building blocks (#29)
    * Add basic, standard and advanced (sync) building blocks
    
    * Standardize placeholder names across building blocks

---
+++ number-insight/ni-standard.py
@@ -0,0 +1,18 @@
+import os
+from os.path import join, dirname
+from pprint import pprint
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '../.env')
+load_dotenv(dotenv_path)
+
+NEXMO_API_KEY = os.getenv('NEXMO_API_KEY')
+NEXMO_API_SECRET = os.getenv('NEXMO_API_SECRET')
+INSIGHT_NUMBER = os.getenv('INSIGHT_NUMBER')
+
+import nexmo
+
+client = nexmo.Client(key=NEXMO_API_KEY, secret=NEXMO_API_SECRET)
+
+insight_json = client.get_standard_number_insight(number=INSIGHT_NUMBER)
+pprint(insight_json)
