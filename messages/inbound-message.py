f83c90b43be9604eb7f8ce755125b80416975695
Tony Bedford <tony.bedford@vonage.com>
2018-10-08 09:52:23 +0100

    Initial versions.

---
+++ messages/inbound-message.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from flask import Flask, request, jsonify
+from pprint import pprint
+
+app = Flask(__name__)
+
+@app.route("/webhooks/inbound-sms", methods=['POST'])
+def inbound_message():
+    data = request.get_json()
+    pprint(data)
+    return "200"
+
+if __name__ == '__main__':
+    app.run(host="www.example.org", port=3000)
+
