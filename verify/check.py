eebd7cc95c9c412b50348714c68009c36f563134
Mark Lewin <mark.lewin@vonage.com>
2018-11-21 17:20:11 +0000

    Add Verify building blocks

---
+++ verify/check.py
@@ -0,0 +1,28 @@
+import os
+from os.path import join, dirname
+import sys
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '../.env')
+load_dotenv(dotenv_path)
+
+NEXMO_API_KEY = os.getenv('NEXMO_API_KEY')
+NEXMO_API_SECRET = os.getenv('NEXMO_API_SECRET')
+
+if len(sys.argv) != 3:
+  print('Please supply request_id and verification code')
+  exit()
+
+REQUEST_ID = sys.argv[1]
+CODE = sys.argv[2]
+
+import nexmo
+
+client = nexmo.Client(key=NEXMO_API_KEY, secret=NEXMO_API_SECRET)
+
+response = client.check_verification(REQUEST_ID, code=CODE)
+
+if response['status'] == '0':
+  print('Verification successful, event_id is %s' % (response['event_id']))
+else:
+  print('Error: %s' % response['error_text'])
