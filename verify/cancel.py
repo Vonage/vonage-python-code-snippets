33a9102983c4f379af0510e828466e2b15f63713
Mark Smith <mark.smith@practicalpoetry.co.uk>
2018-11-30 11:59:34 +0000

    Reformatted with black

--- verify/cancel.py
+++ verify/cancel.py
@@ -3,15 +3,15 @@ from os.path import join, dirname
 import sys
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '../.env')
+dotenv_path = join(dirname(__file__), "../.env")
 load_dotenv(dotenv_path)
 
-NEXMO_API_KEY = os.getenv('NEXMO_API_KEY')
-NEXMO_API_SECRET = os.getenv('NEXMO_API_SECRET')
+NEXMO_API_KEY = os.getenv("NEXMO_API_KEY")
+NEXMO_API_SECRET = os.getenv("NEXMO_API_SECRET")
 
 if len(sys.argv) != 2:
-  print('Please supply request_id')
-  exit()
+    print("Please supply request_id")
+    exit()
 
 REQUEST_ID = sys.argv[1]
 
@@ -21,7 +21,7 @@ client = nexmo.Client(key=NEXMO_API_KEY, secret=NEXMO_API_SECRET)
 
 response = client.cancel_verification(REQUEST_ID)
 
-if response['status'] == '0':
-  print('Cancellation successful')
+if response["status"] == "0":
+    print("Cancellation successful")
 else:
-  print('Error: %s' % response['error_text'])
+    print("Error: %s" % response["error_text"])
--- verify/check.py
+++ verify/check.py
@@ -3,15 +3,15 @@ from os.path import join, dirname
 import sys
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '../.env')
+dotenv_path = join(dirname(__file__), "../.env")
 load_dotenv(dotenv_path)
 
-NEXMO_API_KEY = os.getenv('NEXMO_API_KEY')
-NEXMO_API_SECRET = os.getenv('NEXMO_API_SECRET')
+NEXMO_API_KEY = os.getenv("NEXMO_API_KEY")
+NEXMO_API_SECRET = os.getenv("NEXMO_API_SECRET")
 
 if len(sys.argv) != 3:
-  print('Please supply request_id and verification code')
-  exit()
+    print("Please supply request_id and verification code")
+    exit()
 
 REQUEST_ID = sys.argv[1]
 CODE = sys.argv[2]
@@ -22,7 +22,7 @@ client = nexmo.Client(key=NEXMO_API_KEY, secret=NEXMO_API_SECRET)
 
 response = client.check_verification(REQUEST_ID, code=CODE)
 
-if response['status'] == '0':
-  print('Verification successful, event_id is %s' % (response['event_id']))
+if response["status"] == "0":
+    print("Verification successful, event_id is %s" % (response["event_id"]))
 else:
-  print('Error: %s' % response['error_text'])
+    print("Error: %s" % response["error_text"])
