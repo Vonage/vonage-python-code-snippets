8b7c1a3f1ba03d76ed843df6f603a99a8b6fc52e
Mark Smith <mark.smith@practicalpoetry.co.uk>
2019-03-19 11:03:15 +0000

    Send sms update (#48)
    Update the call to `send_message` so that it checks the response data in a vaguely sensible manner.

--- sms/send-an-sms.py
+++ sms/send-an-sms.py
@@ -2,19 +2,26 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '../.env')
+dotenv_path = join(dirname(__file__), "../.env")
 load_dotenv(dotenv_path)
 
-NEXMO_API_KEY = os.getenv('NEXMO_API_KEY')
-NEXMO_API_SECRET = os.getenv('NEXMO_API_SECRET')
-TO_NUMBER = os.getenv('TO_NUMBER')
+NEXMO_API_KEY = os.getenv("NEXMO_API_KEY")
+NEXMO_API_SECRET = os.getenv("NEXMO_API_SECRET")
+TO_NUMBER = os.getenv("TO_NUMBER")
 
 import nexmo
 
 client = nexmo.Client(key=NEXMO_API_KEY, secret=NEXMO_API_SECRET)
 
-client.send_message({
-    'from': 'Acme Inc',
-    'to': TO_NUMBER,
-    'text': 'A text message sent using the Nexmo SMS API',
-})
+responseData = client.send_message(
+    {
+        "from": "Acme Inc",
+        "to": TO_NUMBER,
+        "text": "A text message sent using the Nexmo SMS API",
+    }
+)
+
+if responseData["messages"][0]["status"] == "0":
+    print("Message sent successfully.")
+else:
+    print(f"Message failed with error: {responseData['messages'][0]['error-text']}")
