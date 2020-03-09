01d980d060accb51419a3ea2ff926a095cc098d8
superdiana <diana.rodriguezmanrique@vonage.com>
2020-03-09 18:08:20 -0400

    Modified the transfer call with inline ncco with new aproach that show how this works in real time

--- voice/transfer-call-inline-ncco.py
+++ voice/transfer-call-inline-ncco.py
@@ -1,40 +1,42 @@
-# Import dependencies
-import nexmo
-import os
+#Import the dependencies
+import nexmo, os
 from os.path import join, dirname
 from dotenv import load_dotenv
+from pprint import pprint
 
-# Load the environment
-envpath = join(dirname(__file__), './.env')
+#Load the environment
+envpath = join(dirname(__file__),'./.env')
 load_dotenv(envpath)
 
-# Init the client
+#Init the client
 print(os.getenv("NEXMO_PRIVATE_KEY"))
 client = nexmo.Client(
-    application_id=os.getenv('NEXMO_APPLICATION_ID'),
-    private_key=os.getenv("NEXMO_PRIVATE_KEY")
+    application_id = os.getenv('NEXMO_APPLICATION_ID'),
+    private_key = os.getenv("NEXMO_PRIVATE_KEY")
 )
 
 response = client.create_call({
-    "to": [{"type": "phone", "number": os.getenv('TO_NUMBER')}],
-    "from": {"type": "phone", "number": os.getenv('FROM_NUMBER')},
-    "ncco": [
-        {
-            "action": "talk",
-            "text": "This is just a text whilst you tranfer to another NCCO"
-        }
-    ]
+  'to': [{'type': 'phone', 'number': os.getenv('TO_NUMBER' }],
+  'from': {'type': 'phone', 'number': os.getenv('FROM_NUMBER' },
+  "ncco": [{"action": "talk", "text": "This is an old NCCO that will be replaced with another one"}]
 })
 
+# the UUID will appear here, just copy it.. Answer the call and then when speech plays paste the UUID
+# And the old NCCO will be replaced with a new one
+print(response)
+
+#Enter the UUID of the inbound call here
+UUID = input("Enter the UUID: ")
+
+#
 response = client.update_call(
-    response["uuid"], {
-        "action": "transfer",
-        "destination": {
+    UUID,{
+        "action":"transfer",
+        "destination":{
             "type": "ncco",
-            "ncco": [{"action": "talk", "text": "hello world"}]
+            "ncco": [{"action": "talk", "text": "This is a new NCCO"}]
         }
     }
 )
 
-
-print(response)
+pprint(response)
