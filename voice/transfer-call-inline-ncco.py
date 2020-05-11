6b31a33eefe8b20fa63f24a186f589209a0b10b2
Diana Rodriguez <61435963+superdiana@users.noreply.github.com>
2020-05-11 11:27:11 -0400

    Eliminating user interaction

--- voice/transfer-call-inline-ncco.py
+++ voice/transfer-call-inline-ncco.py
@@ -1,42 +1,31 @@
-#Import the dependencies
-import nexmo, os
+# Import dependencies
+import nexmo
+import os
 from os.path import join, dirname
 from dotenv import load_dotenv
-from pprint import pprint
 
-#Load the environment
-envpath = join(dirname(__file__),'./.env')
+# Load the environment
+envpath = join(dirname(__file__), './.env')
 load_dotenv(envpath)
 
-#Init the client
-print(os.getenv("NEXMO_PRIVATE_KEY"))
+# Init the nexmo client
 client = nexmo.Client(
-    application_id = os.getenv('NEXMO_APPLICATION_ID'),
-    private_key = os.getenv("NEXMO_PRIVATE_KEY")
+    application_id=os.getenv('NEXMO_APPLICATION_ID'),
+    private_key=os.getenv("NEXMO_PRIVATE_KEY")
 )
 
-response = client.create_call({
-  'to': [{'type': 'phone', 'number': os.getenv('TO_NUMBER')}],
-  'from': {'type': 'phone', 'number': os.getenv('FROM_NUMBER')},
-  "ncco": [{"action": "talk", "text": "This is an old NCCO that will be replaced with another one"}]
-})
+uuid = os.getenv('CALL_UUID')
 
-# the UUID will appear here, just copy it.. Answer the call and then when speech plays paste the UUID
-# And the old NCCO will be replaced with a new one
-print(response)
-
-#Enter the UUID of the inbound call here
-UUID = input("Enter the UUID: ")
-
-#
+#Update the current NCCO with a new one 
 response = client.update_call(
-    UUID,{
-        "action":"transfer",
-        "destination":{
+    uuid, {
+        "action": "transfer",
+        "destination": {
             "type": "ncco",
-            "ncco": [{"action": "talk", "text": "This is a new NCCO"}]
+            "ncco": [{"action": "talk", "text": "This is a transfer action using an inline NCCO"}]
         }
     }
 )
 
-pprint(response)
+
+print(response)
