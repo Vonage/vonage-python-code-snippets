6893d82b2d1ef20d7815b1e1a651c437a687f84d
Tony Bedford <tony.bedford@vonage.com>
2020-05-20 17:10:22 +0100

    Use NEXMO_BRAND_NAME

--- sms/send-an-sms-with-unicode.py
+++ sms/send-an-sms-with-unicode.py
@@ -8,14 +8,20 @@ load_dotenv(dotenv_path)
 NEXMO_API_KEY = os.getenv('NEXMO_API_KEY')
 NEXMO_API_SECRET = os.getenv('NEXMO_API_SECRET')
 TO_NUMBER = os.getenv('TO_NUMBER')
+NEXMO_BRAND_NAME = os.getenv('NEXMO_BRAND_NAME')
 
 import nexmo
 
 client = nexmo.Client(key=NEXMO_API_KEY, secret=NEXMO_API_SECRET)
 
-client.send_message({
-    'from': 'Acme Inc',
+responseData = client.send_message({
+    'from': NEXMO_BRAND_NAME,
     'to': TO_NUMBER,
     'text': 'こんにちは世界',
     'type': 'unicode',
 })
+
+if responseData["messages"][0]["status"] == "0":
+    print("Message sent successfully.")
+else:
+    print(f"Message failed with error: {responseData['messages'][0]['error-text']}")
