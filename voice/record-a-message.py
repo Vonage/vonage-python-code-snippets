a371aac0ab05f9addd5fd5721ff57596deab927f
Vibhuti1402agg <62258495+Vibhuti1402agg@users.noreply.github.com>
2020-10-01 22:51:34 +0530

    Update record-a-message.py

--- voice/record-a-message.py
+++ voice/record-a-message.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python3
 import http
 from pprint import pprint
-
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-
 
 @app.route("/webhooks/answer")
 def answer_call():
     for param_key, param_value in request.args.items():
         print("{}: {}".format(param_key, param_value))
+        
     recording_webhook_url = request.url_root + "webhooks/recording"
     ncco =[
-        {
-            "action": "talk",
-            "text": "Please leave a message after the tone, then press the hash key."
-        },
-        {
-            "action": "record",
-            "endOnKey": "#",
-            "beepStart": "true",
-            "eventUrl": [recording_webhook_url]
-        },
-        {
-            "action": "talk",
-            "text": "Thank you for your message."
-        }
-    ])
+            {
+                "action": "talk",
+                "text": "Please leave a message after the tone, then press the hash key."
+            },
+            {
+                "action": "record",
+                "endOnKey": "#",
+                "beepStart": "true",
+                "eventUrl": [recording_webhook_url]
+            },
+            {
+                "action": "talk",
+                "text": "Thank you for your message."
+            }
+        ]
     return jsonify(ncco)
 
 
