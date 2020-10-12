913fde12467a743bb2ddd8dccc6d27d1bb04f332
Shao Yang Hong <hongsy2006@gmail.com>
2020-10-12 13:19:38 +0800

    Fix envvar in sms/__init__.py

--- sms/__init__.py
+++ sms/__init__.py
@@ -1,5 +1,5 @@
 import vonage
 
 def send(config):
-    sms = vonage.Sms(vonage.Client(key=config['API_KEY'], secret=config['API_SECRET']))
+    sms = vonage.Sms(vonage.Client(key=config['VONAGE_API_KEY'], secret=config['VONAGE_API_SECRET']))
     return sms.send_message({'from': config['FROM_NUMBER'], 'to': config['TO_NUMBER'], 'text': 'Hello from Nexmo!'})
