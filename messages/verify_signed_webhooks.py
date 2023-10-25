17df6d4ba72678bcda6bdac1a8866179a3162649
maxkahan <maxkahan94@gmail.com>
2023-10-25 03:23:18 +0100

    update sample to use the vonage_jwt method

--- messages/verify_signed_webhooks.py
+++ messages/verify_signed_webhooks.py
@@ -1,10 +1,8 @@
-import hashlib
 import os
 from os.path import join, dirname
-from flask import Flask, request, jsonify
-import jwt
+from flask import Flask, request
 from dotenv import load_dotenv
-
+from vonage_jwt.verify_jwt import verify_signature
 
 # Load the environment
 envpath = join(dirname(__file__), "../.env")
@@ -12,53 +10,21 @@ load_dotenv(envpath)
 
 app = Flask(__name__)
 
-port = os.getenv("PORT")
-api_key = os.getenv("NEXMO_API_KEY")  # there may be multiple api_key/sig_secret pairs
-sig_secret = os.getenv("NEXMO_SIG_SECRET")
+VONAGE_SIGNATURE_SECRET = os.getenv("VONAGE_SIGNATURE_SECRET")
 
 
 @app.route("/webhooks/inbound", methods=["POST"])
 def inbound():
-    # Need token after 'Bearer'
-    parts = request.headers["authorization"].split()
-    token = parts[1].strip()
+    # Need to get the JWT after 'Bearer' in the authorization header
+    auth_header = request.headers["authorization"].split()
+    token = auth_header[1].strip()
 
-    # Extract api_key from token payload
-    k = jwt.decode(token, verify=False)["api_key"]
-    # Use k to look up corresponding sig secret
-
-    #### 1. Verify request
-    try:
-        decoded = jwt.decode(token, sig_secret, algorithms="HS256")
-    except Exception as e:
-        print(e)
-        r = '{"msg": "' + str(e) + '"}'
-        return (r, 401)
-
-    #### 2. Verify payload (only needed if using HTTP rather than HTTPS)
-
-    # Obtain transmitted payload hash
-    payload_hash = decoded["payload_hash"]
-
-    # generate hash of request payload
-    payload = request.data  # Obtains request data as binary string
-    h = hashlib.sha256(payload)  # requires binary string
-    hd = h.hexdigest()  # Use hexdigest() and NOT digest()
-
-    # Check the payload hash matches the one we created ourselves from request data
-    if hd != payload_hash:
-        return ('{"msg": "Invalid payload"}', 401)
+    if verify_signature(token, VONAGE_SIGNATURE_SECRET):
+        print('Valid signature')
     else:
-        print("Verified payload")
-    return "OK"
-
-
-@app.route("/webhooks/status", methods=["POST"])
-def status():
-    data = request.get_json()
-    return jsonify(data)
+        print('Invalid signature')
 
 
 if __name__ == "__main__":
     print("Running locally")
-    app.run(host="localhost", port=port)
+    app.run(host="localhost", port=5000)
