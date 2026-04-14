4d06e7aaa43a25184f22c66461f06761d0a53f63
Alvaro Navarro <alvaro.navarro@vonage.com>
2026-04-14 13:50:26 +0200

    feat: add whatsapp zero tap code snippet (#251)

---
+++ verify/send-request-whatsapp-zerotap.py
@@ -0,0 +1,36 @@
+import os
+from os.path import dirname, join
+from pprint import pprint
+
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), "../.env")
+load_dotenv(dotenv_path)
+
+VONAGE_APPLICATION_ID = os.environ.get("VONAGE_APPLICATION_ID")
+VONAGE_PRIVATE_KEY = os.environ.get("VONAGE_PRIVATE_KEY")
+
+VERIFY_BRAND_NAME = os.environ.get("VERIFY_BRAND_NAME")
+VERIFY_NUMBER = os.environ.get("VERIFY_NUMBER")
+VERIFY_FROM_NUMBER = os.environ.get("VERIFY_FROM_NUMBER")
+
+from vonage import Auth, Vonage
+from vonage_verify import (StartVerificationResponse, VerifyRequest,
+                           WhatsappChannel, WhatsappMode)
+
+client = Vonage(
+    Auth(
+        application_id=VONAGE_APPLICATION_ID,
+        private_key=VONAGE_PRIVATE_KEY,
+    )
+)
+
+verify_request = VerifyRequest(
+    brand=VERIFY_BRAND_NAME,
+    workflow=[
+        WhatsappChannel(to=VERIFY_NUMBER, from_=VERIFY_FROM_NUMBER, mode=WhatsappMode.ZERO_TAP),
+    ],
+)
+
+response: StartVerificationResponse = client.verify.start_verification(verify_request)
+pprint(response)
