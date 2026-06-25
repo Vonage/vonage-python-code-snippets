822dc8c2c93e3e90dc33d89a897ed8bb9ea0d4d3
Alvaro Navarro <alvaro.navarro@vonage.com>
2026-06-25 13:21:24 +0200

    feat: add identity insights code snippets (#252)
    * feat: add identity insights code snippets
    
    * Updating INSIGHT_NUMBER var name in line with snippet specs
    
    * Updating SIM Swap snippet vars in line with snippet spec
    
    ---------
    
    Co-authored-by: superchilled <karl@superchilled.co.uk>

---
+++ identity-insights/get-format-insight.py
@@ -0,0 +1,37 @@
+import os
+from os.path import dirname, join
+from pprint import pprint
+
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), "../.env")
+load_dotenv(dotenv_path)
+
+VONAGE_APPLICATION_ID = os.environ.get("VONAGE_APPLICATION_ID")
+VONAGE_PRIVATE_KEY = os.environ.get("VONAGE_PRIVATE_KEY")
+INSIGHT_NUMBER = os.environ.get("INSIGHT_NUMBER")
+IDENTITY_INSIGHTS_API_HOST = os.environ.get("IDENTITY_INSIGHTS_API_HOST")
+
+from vonage import Auth, HttpClientOptions, Vonage
+from vonage_identity_insights import (
+    EmptyInsight,
+    IdentityInsightsRequest,
+    IdentityInsightsResponse,
+    InsightsRequest,
+)
+
+client = Vonage(
+    auth=Auth(
+        application_id=VONAGE_APPLICATION_ID,
+        private_key=VONAGE_PRIVATE_KEY,
+    ),
+    http_client_options=HttpClientOptions(api_host=IDENTITY_INSIGHTS_API_HOST),
+)
+
+request = IdentityInsightsRequest(
+    phone_number=INSIGHT_NUMBER,
+    insights=InsightsRequest(format=EmptyInsight()),
+)
+
+response: IdentityInsightsResponse = client.identity_insights.requests(request)
+pprint(response)
---
+++ identity-insights/get-sim-swap.py
@@ -0,0 +1,39 @@
+import os
+from os.path import dirname, join
+from pprint import pprint
+
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), "../.env")
+load_dotenv(dotenv_path)
+
+VONAGE_APPLICATION_ID = os.environ.get("VONAGE_APPLICATION_ID")
+VONAGE_PRIVATE_KEY = os.environ.get("VONAGE_PRIVATE_KEY")
+INSIGHT_NUMBER = os.environ.get("INSIGHT_NUMBER")
+SIM_SWAP_PERIOD = os.environ.get("SIM_SWAP_PERIOD")
+IDENTITY_INSIGHTS_API_HOST = os.environ.get("IDENTITY_INSIGHTS_API_HOST")
+
+from vonage import Auth, HttpClientOptions, Vonage
+from vonage_identity_insights import (
+    IdentityInsightsRequest,
+    IdentityInsightsResponse,
+    InsightsRequest,
+    SimSwapInsight,
+)
+
+client = Vonage(
+    auth=Auth(
+        application_id=VONAGE_APPLICATION_ID,
+        private_key=VONAGE_PRIVATE_KEY,
+    ),
+    http_client_options=HttpClientOptions(api_host=IDENTITY_INSIGHTS_API_HOST),
+)
+
+request = IdentityInsightsRequest(
+    phone_number=INSIGHT_NUMBER,
+    purpose='FraudPreventionAndDetection',
+    insights=InsightsRequest(sim_swap=SimSwapInsight(period=SIM_SWAP_PERIOD)),
+)
+
+response: IdentityInsightsResponse = client.identity_insights.requests(request)
+pprint(response)
